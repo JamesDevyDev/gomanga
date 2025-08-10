3e75cf8258af80cae6d021f860481ec88a5e6531
James Talamo <jamestalamo4@gmail.com>
2025-08-10 14:20:14 +0800

    landing page (no animation)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,63 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-[100vw] h-[100vh] bg-white relative overflow-hidden">
+      <img
+        src="/landing_assets/imgbg.webp"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+    {/* first load small to normal size */}
+      <img
+        src="/landing_assets/img1.webp"
+        alt="Image 1"
+        className="absolute top-0 left-0 w-full h-full object-contain opacity-[0.8]"
+      />
+      
+      <img
+        src="/landing_assets/img2.webp"
+        alt="Image 2"
+        className="absolute top-0 left-0 w-full h-full object-contain opacity-[0.8]"
+      />
+      <img
+        src="/landing_assets/img3.webp"
+        alt="Image 3"
+        className="absolute top-0 left-0 w-full h-full object-contain opacity-[0.8]"
+      />
+      <img
+        src="/landing_assets/img4.webp"
+        alt="Image 4"
+        className="absolute top-0 left-0 w-full h-full object-contain opacity-[0.8]"
+      />
+      <img
+        src="/landing_assets/img5.webp"
+        alt="Image 5"
+        className="absolute top-0 left-0 w-full h-full object-contain opacity-[0.8]"
+      />
+
+      {/* Logo */}
+      <div className='absolute top-0 left-0 w-full h-full  flex flex-col items-center justify-center'>
+        <div className='w-[400px] h-400px] '>
+          <img
+            src="/landing_assets/logo.png"
+            alt="Logo"
+            className=" object-contain relative bg-none "
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className='bg-black w-[400px] h-[80px] relative rounded-xl flex items-center justify-center cursor-pointer hover:scale-105 transition-transform duration-200'>
+          <div className='w-[98%] h-[90%] bg-white rounded-xl flex items-center justify-center text-black font-bold text-[40px] cursor-pointer'>
+            Start Reading
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+
     </div>
   );
 }
